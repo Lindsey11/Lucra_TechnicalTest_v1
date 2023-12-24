5492c3e1ac1b983cc980f4ee59ce385e6b07ad7b
Lindsey11 <drewlindsey017@gmail.com>
2023-12-24 11:20:07 +0200

    Updated to make sure all error are handled to inform user and prevent fall over

--- ImagePortal.UI/Components/ImageListComponent.razor.cs
+++ ImagePortal.UI/Components/ImageListComponent.razor.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Components;
 using System.Linq;
 using System.Net.WebSockets;
 using System.Text.Json;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ImagePortal.UI.Components
 {
@@ -12,7 +13,7 @@ namespace ImagePortal.UI.Components
         [Inject]
         private IImagePortalAPIClient _apiClient { get; set; }
         public List<UIImageDataViewModel> Images { get; set; } = new List<UIImageDataViewModel>();
-        public Dictionary<int, string> tags { get; set; } = new Dictionary<int, string>();
+        private CustomDialog dialog { get; set; }
         public bool IsLoadding { get; set; }
         public int pageNumber { get; set; } = 1;
         public int pageSize { get; set; } = 8;
@@ -28,39 +29,23 @@ namespace ImagePortal.UI.Components
                 IsLoadding = true;
                 var data = await _apiClient.GetImages(pageNumber, pageSize);
 
-                Images = data.data;
-
-                //await BuildListFilerTags(Images);
+                if (data.success)
+                {
+                    Images = data.data;
+                }
+                else
+                {
+                    dialog.Show("Error", $"Error {data.messagae}", false);
+                }
 
                 IsLoadding = false;
             }
             catch (Exception error)
             {
                 IsLoadding = false;
-                Console.Write(error.Message);
+                dialog.Show("Error", $"Error {error.Message}", false);
             }
         }
-
-       //public async Task BuildListFilerTags(List<UIImageDataViewModel> Images)
-       // {
-       //     foreach (var image in Images)
-       //     {
-       //         if(!string.IsNullOrEmpty(image.imageMetaDataViewModel.tags))
-       //         {
-       //             var rawtag = image.imageMetaDataViewModel.tags.Split(",");//JsonSerializer.Deserialize<List<ImageTags>>(image.imageMetaDataViewModel.tags);
-       //             foreach (var item in rawtag)
-       //             {
-
-       //                 if (!tags.ContainsValue(item))
-       //                 {
-       //                     tags.Add(image.imageId, item);
-       //                 }
-       //             }
-       //         }
-            
-            
-       //     }
-       // }
 
         public async Task LoadNextBatch()
         {
